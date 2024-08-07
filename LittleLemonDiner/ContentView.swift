4d169d67f1b1193a73927f2f53fa5c7742a42166
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-07 17:02:06 -0600

    Initial Commit

---
+++ LittleLemonDiner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemonDiner
+//
+//  Created by Bidisha Biswas on 8/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemonDiner/LittleLemonDinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonDinerApp.swift
+//  LittleLemonDiner
+//
+//  Created by Bidisha Biswas on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonDinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
