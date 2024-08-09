ae5b536772097a30b0156e433f7a7f429b363280
Bidisha Biswas <bdsh.14@gmail.com>
2024-08-09 13:57:02 -0600

    Ingredients struct

---
+++ LittleLemonDiner/Models/Ingredient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingredient.swift
+//  LittleLemonDiner
+//
+//  Created by Bidisha Biswas on 8/8/24.
+//
+
+import Foundation
+
+enum Ingredient: String, CaseIterable {
+	case spinach
+	case broccoli
+	case carrot
+	case pasta
+	case tomatoSauce
+}
